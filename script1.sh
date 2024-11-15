e3094c66970dae2279c6c5e4b50b472e43387490
monica368 <monicakale09@gmail.com>
2024-11-15 14:34:13 +0530

    This is my commoit one

--- script1.sh
+++ script1.sh
@@ -1,5 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
-echo "we are from batch-26 learning devops"
-echo "i am learning Devops"
+echo "hii all i am learning devops"
 
